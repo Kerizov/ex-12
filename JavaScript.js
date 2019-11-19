551e6a2457c307320686fc79587961f8621afd31
Kerizov <stepanok980@gmail.com>
2019-11-19 14:21:07 +0300

    ex 12

--- JavaScript.js
+++ JavaScript.js
@@ -62,7 +62,7 @@ let double = count.bind(2)
 console.log(double(3));
 console.log(double(10));*/
 
-let btn = document.querySelector('button');
+/*let btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
 	console.log(this);
@@ -71,7 +71,7 @@ btn.addEventListener('click', function() {
 		console.log(this);
 	}
 	showThis();
-});
+});*/
 
 
 
@@ -80,3 +80,95 @@ btn.addEventListener('click', function() {
 // 2) метод обьекта - this = обьект
 // 3) конструктор (new) - this = новый созданный обьект  
 // 4) указание конкретного контекста - call, apply, bind
+
+
+let name = "Ivan",
+	age = 30,
+	mail = 'ex@gmail.com';
+
+document.write(`Пользователю ${name} ${age} лет. Его почта: ${mail}`);
+
+
+function makeArray() {
+	var items = [];
+
+	for (let/*var совсем другой вывод в консоли*/ i = 0; i < 10; i++) {
+		var item = function() {
+			console.log(i);
+		};
+		items.push(item);
+	}
+
+	return items;
+}
+
+var arr = makeArray();
+
+arr[1]();
+arr[3]();
+arr[7]();
+
+let fun = () => {
+	console.log(this)
+};
+
+//fun();
+
+let obj = {
+	number: 5,
+	sayNumber: function() {
+		let say = () => {
+			console.log(this);
+		};
+		say();
+	}
+};
+
+obj.sayNumber();
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+	let show = () => {
+		console.log(this);
+	};
+	show();
+});
+
+function calcOrDouble(number, basis = 2) {//= 2 ES6
+	//basis = basis || 2; ES5
+	console.log(number*basis);
+}
+calcOrDouble(3,5);
+calcOrDouble(6);
+
+class Rectangle {
+	constructor(height, width = 20) {
+		this.height = height;
+		this.width = width;
+
+	}
+	calcArea() {
+		return this.height * this.width;
+	}
+}
+
+const square = new Rectangle(10);
+
+console.log(square.calcArea());
+
+let video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejournal', 'blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+	console.log(a + b + c);
+}
+let numbers = [2, 5 ,7];
+
+log(...numbers);
--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-class User {
+/*class User {
 	constructor(name, id) {
 		this.name = name;
 		this.id = id;
@@ -16,4 +16,49 @@ let ivan = new User('Ivan', 25);
  console.log(ivan);
  console.log(alex);
  ivan.hello();
- alex.hello();
+ alex.hello();*/
+
+ /*class Options {
+ 	constructor(height, width, bg, fontSize, textAlign) {
+ 		this.height = height;
+ 		this.width = width;
+ 		this.bg = bg;
+ 		this.fontSize = fontSize;
+ 		this.textAlign = textAlign;
+ 	}
+ 		createDiv() {
+ 			let elem = document.createElement('div');
+ 			document.body.appendChild(elem);
+ 			let param = `Высота:${this.height}px; ширина:${this.width}px; Цвет фона:${this.bg}px; Размер шрифта:${this.fontSize}px; Выравнивание текста:${this.textAlign}px`;
+ 			elem.style.cssText = param;
+ 		}
+ 		
+ }
+ const item = new Options("1000px", "2000px", "green", "20px", "center");
+
+ item.createDiv();*/
+
+ class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	newDiv() {
+		let block = document.createElement('div');
+		document.body.append(block);
+		let p = `height:${this.height}px;
+		 			 width:${this.width}px;
+		 			 background-color:${this.bg};
+		 			 font-size:${this.fontSize}px; 
+		 			 text-align:${this.textAlign}`;
+		block.style.cssText = p;
+	}
+}
+
+const item = new Options(100, 200, "green", 20, "center");
+
+item.newDiv();
